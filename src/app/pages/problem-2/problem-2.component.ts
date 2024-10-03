69f046e661894503aec3a7b16ef5a03369e31b10
JAL <jalejandromogro1@gmail.com>
2024-10-03 07:04:04 -0400

    chore: set up project environment with initial configuration

---
+++ src/app/pages/problem-2/problem-2.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './problem-2.component.html',
+  styleUrl: './problem-2.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Problem2Component {
+
+}
