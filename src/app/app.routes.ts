abf0604dcf502e4ac7cb553921aace1cc44d6cff
jal <jalejandromogro1@gmail.com>
2024-10-04 17:01:28 -0400

    feat: add routes: problem-1 | problem-2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'problem-1',
+    loadComponent: () =>
+      import('./pages/problem-1/problem-1.component').then(
+        (m) => m.Problem1Component
+      ),
+  },
+  {
+    path: 'problem-2',
+    loadComponent: () =>
+      import('./pages/problem-2/problem-2.component').then(
+        (m) => m.Problem2Component
+      ),
+  },
+];
