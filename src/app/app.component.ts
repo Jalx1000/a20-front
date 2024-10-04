1a9f3ff0d68db9e88b5985464c08d44cf09d2ee1
jal <jalejandromogro1@gmail.com>
2024-10-04 18:19:32 -0400

    feat: haciendo constructor function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeadersComponent } from './components/headers/headers.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,HeadersComponent],
+  imports: [RouterOutlet, RouterLink, HeadersComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/problem-1/problem-1.component.ts
+++ src/app/pages/problem-1/problem-1.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-problem-1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, BrowserModule],
   templateUrl: './problem-1.component.html',
   styleUrl: './problem-1.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Problem1Component {
+  board: string[][] = [];
 
+  constructor() {
+    this.createBoard();
+  }
+
+  createBoard() {
+    for (let row = 0; row < 8; row++) {
+      this.board[row] = [];
+      for (let col = 0; col < 8; col++) {
+        // Alternar color de la casilla
+        const isBlack = (row + col) % 2 === 1;
+        this.board[row][col] = isBlack ? 'black' : 'white';
+      }
+    }
+  }
 }
